5361a612b8d98ec59473298b81eb18382c4d683c
Ezequiel Nielsen Hillen <ezenh87@gmail.com>
2023-11-06 21:51:24 -0300

    test 5

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
-window.addEventListener('resize', () => {
-  // We execute the same script as before
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
+// window.addEventListener('resize', () => {
+//   // We execute the same script as before
+//   let vh = window.innerHeight * 0.01;
+//   document.documentElement.style.setProperty('--vh', `${vh}px`);
+// });
 
 //DETECCION DE POSICION DE SECTIONS PARA ANIMACIONES
 const main = document.getElementById("rest")
